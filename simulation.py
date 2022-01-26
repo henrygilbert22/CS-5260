28148c6e2f05c7e9e3ce7b2a4d99d5f3d46082fc
henrygilbert22 <77060624+henrygilbert22@users.noreply.github.com>
2022-01-26 16:39:57 -0500

    update

--- simulation.py
+++ simulation.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import pandas as pd
 
 @dataclass
 class Country:
@@ -10,6 +11,9 @@ class Country:
     r21: int
     r22: int
     r23: int
+    r21w: int       # Waste for r21
+    r22w: int       # Waste for r22
+    r22w: int       # Waste for r23
     
 @dataclass
 class ResourceWeights:
@@ -23,8 +27,32 @@ class ResourceWeights:
     r21w: int       # Waste for r21
     r22w: int       # Waste for r22
     r22w: int       # Waste for r23
+
+
+class Simulation:
+    
+    countries: list[Country]
+    r_weights: ResourceWeights
+    
+    def __init__(self) -> None:
+        pass
+    
+    def load(self):
+        
+        self.load_countries('Example-Initial-Countries.xlsx')
+        
+        
+    def load_countries(self, file_name: str):
+    
+        df = pd.read_excel(file_name)
+        
+        print(df)
+        
+        
+        
 def main():
-    return
+    
+    s = Simulation()
 
 if __name__ == '__main__':
     main()
