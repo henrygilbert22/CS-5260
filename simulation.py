d6fca0214031313f939a5f0aa83ea66170cb535d
henrygilbert22 <77060624+henrygilbert22@users.noreply.github.com>
2022-01-26 17:13:53 -0500

    Updated naming

--- simulation.py
+++ simulation.py
@@ -5,28 +5,28 @@ import pandas as pd
 class Country:
     
     name: str
-    r1: int
-    r2: int
-    r3: int
-    r21: int
-    r22: int
-    r23: int
-    r21w: int       # Waste for r21
-    r22w: int       # Waste for r22
-    r22w: int       # Waste for r23
+    population: int
+    metalic_elm: int
+    timber: int
+    metalic_alloys: int
+    electronics: int
+    housing: int
+    waste1: int = 0     # Waste for metalic_alloys
+    waste2: int = 0     # Waste for electronics
+    waste3: int = 0     # Waste for housing
     
 @dataclass
 class ResourceWeights:
     
-    r1: int
-    r2: int
-    r3: int
-    r21: int
-    r22: int
-    r23: int
-    r21w: int       # Waste for r21
-    r22w: int       # Waste for r22
-    r22w: int       # Waste for r23
+    population: float
+    metalic_elm: float
+    timber: float
+    metalic_alloys: float
+    electronics: float
+    housing: int
+    waste1: float = 0.0     # Waste for metalic_alloys
+    waste2: float = 0.0     # Waste for electronics
+    waste3: float = 0.0     # Waste for housing
 
 
 class Simulation:
@@ -34,25 +34,43 @@ class Simulation:
     countries: list[Country]
     r_weights: ResourceWeights
     
-    def __init__(self) -> None:
-        pass
+    countries_file_name: str
+    weights_file_name: str
+    
+    def __init__(self, countries_file_name: str, weights_file_name: str) -> None:
+        
+        self.countries_file_name = countries_file_name
+        self.weights_file_name = weights_file_name
+        
+        self.countries = []
+        
+        self.load()
     
     def load(self):
         
-        self.load_countries('Example-Initial-Countries.xlsx')
+        self.load_countries(self.countries_file_name)
+        self.load_weights(self.weights_file_name)
         
         
+    def load_weights(self, file_name: str):
+        
+        df = pd.read_excel(file_name)
+        args = pd.Series(df.Weight.values).to_list()
+        
+        self.r_weights = ResourceWeights(*args)        
+           
+           
     def load_countries(self, file_name: str):
     
         df = pd.read_excel(file_name)
         
-        print(df)
-        
+        for index, row in df.iterrows(): 
+           self.countries.append(Country(*row.values))
         
         
 def main():
     
-    s = Simulation()
+    s = Simulation('Example-Initial-Countries.xlsx', 'Example-Sample-Resources.xlsx')
 
 if __name__ == '__main__':
     main()
