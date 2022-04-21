2debd7db612b44cd990abd773f8ea17469559547
henrygilbert22 <77060624+henrygilbert22@users.noreply.github.com>
2022-04-21 13:16:42 -0400

    Update

--- P2/mlruns/2/91f4c32dcf4249a2a04e5a87ad04844d/artifacts/model.py
+++ P2/mlruns/2/91f4c32dcf4249a2a04e5a87ad04844d/artifacts/model.py
@@ -79,11 +79,11 @@ class Model():
         learning_rate = 0.7         
         discount_factor = 0.618     
 
-        MIN_REPLAY_SIZE = 1500      
+        MIN_REPLAY_SIZE = 500      
         if len(replay_memory) < MIN_REPLAY_SIZE:        # Only do this function when we've gone through atleast 1000 steps?
             return 0, 0
 
-        data_set_size = 1000     # Getting random 128 batch sample from 
+        data_set_size = 256     # Getting random 128 batch sample from 
         mini_batch = random.sample(replay_memory, data_set_size)       # Grabbing said random sample
         current_states = np.array([transition[0] for transition in mini_batch])     # Getting all the states from your sampled mini batch, because index 0 is the observation
         current_qs_list = self.model.predict(current_states)     # Predict the q values based on all the historical state
@@ -107,11 +107,11 @@ class Model():
             Y.append(current_qs)            #
         
         train_data = tf.data.Dataset.from_tensor_slices((X, Y))
-        train_data = train_data.batch(16, drop_remainder=True)
+        train_data = train_data.batch(64, drop_remainder=True)
         options = tf.data.Options()
         options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.OFF
 
-        history = self.model.fit(train_data, batch_size=16, verbose=2, shuffle=True, epochs=1)      
+        history = self.model.fit(train_data, batch_size=64, verbose=2, shuffle=True, epochs=100)      
         return history.history['loss'][-1], history.history['accuracy'][-1]
     
     def update_target(self):
